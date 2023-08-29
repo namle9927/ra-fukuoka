db9b5c670d0c1ba034440abcc0fc99ced1c084d1
namle9927 <nam19992706@gmail.com>
2023-08-29 21:59:38 +0900

    add file

---
+++ bai6/index.js
@@ -0,0 +1,41 @@
+// document.write("xin chao ra buoi toi!");
+// var fullName="le ngoc nam";
+// var a=5;
+// var a=15;
+// document.write(a)
+// let b=6;
+// b=16;
+// document.write(b);
+// const PI=3.14;
+// PI=34
+// let a=6;
+// console.log(typeof(a));
+// let b= "6" //chuoi string
+// console.log(typeof(b));
+// let c="nam"/3;
+// console.log(Number.isNaN(c));
+// // let cu1=+prompt("nhap tuoi cu1");
+// // let cu2=+prompt("nhap tuoi cu2");
+// // console.log("cu1",cu1);
+// // document.write(cu1+cu2);
+// console.log(5>7);
+// document.write(5>7);
+// console.log("555",typeof(5>3));
+// let number=null;
+// console.log(number);
+// let age=prompt("nhap so tuoi cua ban");
+// document.write("so tuoi cua ban la", age);
+// let confirmAge=confirm("bạn đủ 18 tuổi chưa?");
+// if(confirmAge==true){
+//     document.write("bạn có thể mua vé xem phim!");
+// }else{
+//     document.write("bạn không thể mua vé xem phim!");}
+    // alert("thang ngu");
+    // let number=prompt("mời bạn nhập mật khẩu ");
+    //     if(number!="123456"){
+    //         alert("bạn vừa nhập sai mật khẩu");
+    //     }else{
+    //         alert("chào mừng khánh")
+    //     }
+    let a=3;
+    console.log
