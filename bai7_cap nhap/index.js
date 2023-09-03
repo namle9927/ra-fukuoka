2f0b617228a07b63fd2968236f25671d581c12ba
namle9927 <nam19992706@gmail.com>
2023-09-03 15:28:38 +0900

    addfile

---
+++ bai7_cap nhap/index.js
@@ -0,0 +1,61 @@
+
+// DK if
+// if (5 < 6) {
+//     console.log('5 lon hon 4');
+// }
+// if-else
+/* if ( 3 > 5 ) {
+console.log('dieu kien dung');  
+}
+else { 
+    console.log('dieu kien sai');
+} */
+/* if else bac thang
+if (3 > 5) {
+    console.log("3 lớn hơn 5");
+} else if (4 > 8) {
+    console.log("4 lớn hơn 8");
+} else if (5 > 3) {
+    console.log("5 lớn hơn 3");
+    // dừng lại luôn
+} else if (5 > 2) {
+    console.log("5 lớn hơn 2");
+} else {
+    console.log("không có trường hợp nào đúng!"); */
+    // `` dấu template string
+    // let a=16;
+    // console.log(`${a} là số chẵn`);
+    // switch case
+    // let day = prompt("mời bạn nhập ngày trong tuần");
+
+    //     switch (day) {
+    //         // "2"
+    //         case "2":// so sánh "2"=="2"
+    //             console.log("ngày thứ 2 trong tuần");
+    //         case "3":
+    //             console.log("ngày thứ 3 trong tuần");
+    //             break;
+    //         default:
+    //             console.log("ngày khác không phải thứ 2 ,3");  
+    //     }
+/*     for (let i = 1; i < 11 ; i++) {
+        if (i%2==0){
+console.log(`i=${i}`);
+        }
+    } */
+
+/*     for (let i = 1; i < 3; i++) {
+        console.log("i",i);//1
+       for (let j = 0; j < 3; j++) {
+            console.log("j",j);
+            //0 1 2 
+            //******
+        
+       }
+       console.log("*********");
+    } */
+    /* let text = prompt('moi nhap mat khau');
+    while(text!="123456"){
+        text=prompt('moi nhap lai mat khau')
+    }
+ */
